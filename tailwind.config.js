b1cc3665be62c53bb62bdb4826707b2c1bee6438
dejauls <rajauls178@gmail.com>
2024-09-01 14:59:53 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+   
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      spacing:{
+        "big":"48rem"
+      },
+      colors: {
+        'krim': '#F5EAB9', 
+      },
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif'],
+      sans: ['Poppins', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
+
+
